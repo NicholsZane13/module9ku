8315f18533000bc86435d8eeabecc33565879501
nicholsz <nichols.zane16@gmail.com>
2022-11-14 17:01:28 -0600

    completed prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { messages } = require('statuses');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+    .prompt(
+        [
+            {
+                type: 'input',
+                message: 'What is the name of the project?',
+                title: "title",
+
+            }
+        ]
+    )
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
